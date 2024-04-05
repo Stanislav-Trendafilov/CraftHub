77529d4d6d2d9feec57bd15b9f549e16d97129f8
Stanislav Trendafilov <114089296+Stanislav-Trendafilov@users.noreply.github.com>
2024-04-05 14:41:22 +0300

    Update CourseParticipant.cs

--- CraftHub/CraftHub.Infrastructure/Data/Models/CourseParticipant.cs
+++ CraftHub/CraftHub.Infrastructure/Data/Models/CourseParticipant.cs
@@ -1,6 +1,25 @@
-﻿namespace CraftHub.Infrastructure.Data.Models
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace CraftHub.Infrastructure.Data.Models
 {
+	[Comment("This is entity which makes possible many to many relation between classes.")]
 	public class CourseParticipant
 	{
+		[Comment("Identifier of current course.")]
+		[Required]
+		public int CourseId { get; set; }
+
+		[ForeignKey(nameof(CourseId))]
+		public Course Course { get; set; } = null!;
+
+		[Comment("Identifier of the participant who will go on the course.")]
+		[Required]
+		public string ParticipantId { get; set; } = string.Empty;
+
+		[ForeignKey(nameof(ParticipantId))]
+		public IdentityUser Participant { get; set; } = null!;
 	}
 }
