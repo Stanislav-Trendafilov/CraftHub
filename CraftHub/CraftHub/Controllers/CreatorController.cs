01e8fe17fa27e16f51e5c0e8c0d87387f7a59a98
Stanislav Trendafilov <114089296+Stanislav-Trendafilov@users.noreply.github.com>
2024-04-10 11:09:19 +0300

    Changes

---
+++ CraftHub/CraftHub/Controllers/CreatorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CraftHub.Controllers
+{
+	public class CreatorController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
