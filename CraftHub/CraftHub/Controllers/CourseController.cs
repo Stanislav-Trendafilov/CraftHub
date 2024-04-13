62346a1fe079a8a934458bb6d5c40bfb8ba877dd
Stanislav Trendafilov <114089296+Stanislav-Trendafilov@users.noreply.github.com>
2024-04-13 22:22:12 +0300

    add course controller

---
+++ CraftHub/CraftHub/Controllers/CourseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CraftHub.Controllers
+{
+	public class CourseController : BaseController
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
