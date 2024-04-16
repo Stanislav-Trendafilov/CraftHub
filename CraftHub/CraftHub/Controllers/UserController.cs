2aafd2dfd62e45ee57a3b4ac8c2201f42720837d
Stanislav Trendafilov <114089296+Stanislav-Trendafilov@users.noreply.github.com>
2024-04-17 00:12:50 +0300

    Add Admin Configuration

--- CraftHub/CraftHub/Controllers/UserController.cs
+++ CraftHub/CraftHub/Controllers/UserController.cs
@@ -1,16 +1,23 @@
 ﻿using CraftHub.Areas.Admin.Controllers;
+using CraftHub.Attributes;
 using CraftHub.Core.Contracts;
+using CraftHub.Core.Models.Product;
+using CraftHub.Core.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CraftHub.Controllers
 {
     public class UserController : AdminBaseController
     {
+        private IProductService productService;
+        private ICourseService courseService;
 
         private readonly IUserService _users;
-        public UserController(IUserService users)
+        public UserController(IUserService users, IProductService productService, ICourseService courseService)
         {
             _users = users;
+            this.productService = productService;
+            this.courseService = courseService;
         }
 
         [Route("User/All")]
@@ -20,5 +27,19 @@ namespace CraftHub.Controllers
 
             return View(users);
         }
+
+
+        //[HttpPost]
+        //public async Task<IActionResult> AddProductCategory(int productCategory,int id)
+        //{
+        //    if (await productService.CategoryExistsAsync(id) == true)
+        //    {
+        //        ModelState.AddModelError(nameof(id), "Category already exists");
+        //    }
+
+        //    int newProductId = await productService.CreateAsync(model, creatorId ?? 0);
+
+        //    return RedirectToAction(nameof(My));
+        //}
     }
 }
