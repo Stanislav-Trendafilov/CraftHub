5b905d58a15c374e758d58e228f6c61961459273
Stanislav Trendafilov <114089296+Stanislav-Trendafilov@users.noreply.github.com>
2024-04-17 23:56:15 +0300

    Remove Useless Usages

--- CraftHub/CraftHub.Tests/UnitTests/UserServiceTests.cs
+++ CraftHub/CraftHub.Tests/UnitTests/UserServiceTests.cs
@@ -2,11 +2,6 @@
 using CraftHub.Core.Services;
 using CraftHub.Infrastructure.Data.Models;
 using Microsoft.AspNetCore.Identity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CraftHub.Tests.UnitTests
 {
